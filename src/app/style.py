e9aefd80ad1ee2488d458367e9c3bfe29df86e43
Jonathan ABADIE <abadiejoni@hotmail.fr>
2023-09-04 11:27:28 +0200

    Mk grph (#2)
    * Add streamlit conversation using streamlit and langchain
    
    * pip freeze requirements.txt
    
    * 1st layer data extraction, tocken cpmrsn
    
    * req update, test number of tocken in resp
    
    * update readme
    
    * changing the python version to 3.9.6
    
    * recursive domain extraction
    
    * Example datascience topics file
    
    * Add test file for extract data
    
    * mk graph
    
    * Add underscore handling
    
    * make graph, nx and neo4j
    
    * cytoscape graph
    
    * Add question mining
    
    * Add UI
    
    * Add spinner when grading
    
    * Add show answer button
    
    * Fix UI not updating on index change
    
    * data science paper graph
    
    * add page rank to vis
    
    * graph exploration
    
    * wikiversity
    
    * wiki agent
    
    * wikiversity
    
    * test server of neo4j
    
    * pdf extraction exmpl
    
    * Better streamlit + PaperGPT
    
    ---------
    
    Co-authored-by: Jonabadie <abadiejoinn@hotmail.fr>
    Co-authored-by: antahiap <antahia@yahoo.com>

---
+++ src/app/style.py
@@ -0,0 +1,59 @@
+import streamlit as st
+
+css='''
+<style>
+    section.main>div {
+        padding-bottom: 1rem;
+    }
+
+    /* Scrollable columns */
+    [data-testid="column"] {
+        overflow-y: auto;
+        overflow-x: hidden;
+        height: 80vh;
+    }
+    
+    .tooltip {
+        position: relative;
+        display: inline-block;
+        border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+    }
+
+    /* Tooltip text */
+    .tooltip .tooltiptext {
+        font-size: 10px;
+        visibility: hidden;
+        width: 300px;
+        background-color: black;
+        color: #fff;
+        text-align: center;
+        padding: 5px;
+        border-radius: 6px;
+
+        bottom: 100%;
+        left: 50%;
+        margin-left: -150px; /* Use half of the width, to center the tooltip */
+        
+        /* Position the tooltip text - see examples below! */
+        position: absolute;
+        z-index: 1;
+    }
+
+    /* Show the tooltip text when you mouse over the tooltip container */
+    .tooltip:hover .tooltiptext {
+        visibility: visible;
+    }
+
+    /* Tooltip arrow */
+    .tooltip .tooltiptext::after {
+        content: " ";
+        position: absolute;
+        top: 100%; /* At the bottom of the tooltip */
+        left: 50%;
+        margin-left: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: black transparent transparent transparent;
+    }
+</style>
+'''
--- src/openai_api.py
+++ src/openai_api.py
@@ -5,18 +5,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class OpenAIApi():
-    def __init__(self, model, timeout=10):
+    def __init__(self, model="gpt-3.5-turbo", timeout=10):
         openai.api_key = os.getenv("OPENAI_API_KEY")
         self.model = model
         self.total_cost = 0
         self.timeout = timeout
 
     MAX_RETRIES = 3
-    def call_api(self, prompt):
+    def call_api(self, prompt, model=None):
+        if model is None:
+            model = self.model
         for i in range(self.MAX_RETRIES):
             try:
                 response = openai.ChatCompletion.create(
-                    model=self.model,
+                    model=model,
                     messages=[{"role": "user", "content": prompt}],
                     timeout=self.timeout,
                 )
@@ -24,7 +26,7 @@ class OpenAIApi():
                 break
             except Exception as e:
                 print("Error: ", str(e))
-                if i == self.MAX_RETRIES - 1:
+                if i >= self.MAX_RETRIES - 1:
                     raise Exception("Max retries exceeded")
         return self._format_response(response)
     
@@ -35,6 +37,9 @@ class OpenAIApi():
         self.total_cost += cost
         return content, cost, tokens
     
-    TOKEN_COST = {'gpt-3.5-turbo': 0.002 / 1000}
+    TOKEN_COST = {
+        'gpt-3.5-turbo': 0.002 / 1000,
+        "gpt-3.5-turbo-16k": 0.006 / 1000,
+    }
     def _cost_tokens(self, n_tokens):
         return n_tokens * self.TOKEN_COST[self.model]
